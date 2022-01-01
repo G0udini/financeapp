372b0f58824ac31880f4191d53b5d7928da43d08
G0udini <ruslan.shabunin2010@yandex.ru>
2022-01-01 18:49:14 +0300

    add hintings and rework stock search func

--- stock/services.py
+++ stock/services.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import finnhub
 import os
 import datetime
@@ -8,28 +9,41 @@ class FinnHub:
         self.api_key = os.getenv("API_KEY")
         self.client = finnhub.Client(api_key=self.api_key)
 
-    def get_current_stock_price(self, symb):
+    def get_current_stock_price(self, symb: str) -> Optional[float]:
         finn_data = self.client.quote(symb)
-        return finn_data["c"]
+        if finn_data["c"]:
+            return finn_data["c"]
+        return
 
-    def get_candle_stock_price(self, symb):
+    def get_candle_stock_price(self, symb: str) -> Optional[list[float]]:
         current_time = datetime.datetime.utcnow()
         end_interval = int(current_time.timestamp())
         start_interval = int((current_time - datetime.timedelta(days=365)).timestamp())
         stock_history = self.client.stock_candles(
             symb, "W", start_interval, end_interval
         )
+        if stock_history["s"] == "no_data":
+            return
         stock_history = stock_history["c"]
         return stock_history
 
-    def get_stock_list(self, symb):
+    def get_stock_by_search(self, symb: str) -> Optional[list[dict[str, str]]]:
         stock_list = self.client.symbol_lookup(symb)
-        stock_list = stock_list["result"][:10]
-        return [
-            {"company": stock["description"], "symb": stock["symbol"]}
-            for stock in stock_list
-        ]
+        stock_company = stock_list["result"][0]
+        if stock_company:
+            return {
+                "company": stock_company["description"],
+                "symb": stock_company["symbol"],
+            }
+        return
 
-    def get_company_info(self, symb):
+    def get_company_info(self, symb: str) -> Optional[dict[str, str]]:
         company = self.client.company_profile2(symbol=symb)
-        return {company["country"], company["ipo"], company["name"], company["ticker"]}
+        if company:
+            return {
+                company["country"],
+                company["ipo"],
+                company["name"],
+                company["ticker"],
+            }
+        return
